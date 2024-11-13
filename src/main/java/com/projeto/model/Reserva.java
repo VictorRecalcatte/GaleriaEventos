180966003b1927848305aace7c8f97ec3c98d90e
victorrecalcatte <victoraugusto.var@gmail.com>
2024-11-13 10:49:28 -0300

    Sistema de Reservas

---
+++ src/main/java/com/projeto/model/Reserva.java
@@ -0,0 +1,58 @@
+package com.projeto.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Reserva {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+
+    private long id;
+    private long id_usuario;
+    private long id_evento;
+    private String nome_evento;
+    private String nome_usuario;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(long id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public long getId_evento() {
+        return id_evento;
+    }
+
+    public void setId_evento(long id_evento) {
+        this.id_evento = id_evento;
+    }
+
+    public String getNome_evento() {
+        return nome_evento;
+    }
+
+    public void setNome_evento(String nome_evento) {
+        this.nome_evento = nome_evento;
+    }
+
+    public String getNome_usuario() {
+        return nome_usuario;
+    }
+
+    public void setNome_usuario(String nome_usuario) {
+        this.nome_usuario = nome_usuario;
+    }
+}
